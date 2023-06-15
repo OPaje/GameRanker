911921c12870ac53b0cdd785aa787a369c72194b
OPaje <jecssilva@gmail.com>
2023-06-15 14:42:09 -0300

    criando o card e colocando os jogos nele

--- src/screens/VotesScreen.tsx
+++ src/screens/VotesScreen.tsx
@@ -1,10 +1,28 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, {useEffect, useState} from "react";
+import {View, Text, StyleSheet, ScrollView} from "react-native";
+import {GameCard} from '../components/Votes/GameCard'
+import {Header} from '../components/Votes/Header'
+import {clientGetGames} from '../api/api'
 
 export function VotesScreen(){
+
+    const [gameList, setGameList] = useState([{}]) // hook
+
+    useEffect(() =>{
+
+        (async () =>{
+            const response = await clientGetGames()
+            setGameList(response)
+        })()
+
+    },[])
+
     return (
         <View style = {styles.container}>
-            <Text>Exodia Obliterar</Text>
+            <Header/>
+            <ScrollView style={styles.gameArea}>
+                {gameList.map(game => GameCard(game))}
+            </ScrollView>
         </View>
     )
 }
@@ -12,10 +30,16 @@ export function VotesScreen(){
 const styles = StyleSheet.create({
         container:{
             flex: 1,
-            color: 'white',
+            color: '#fff',
             alignItems: 'center',
             justifyContent: 'center',
             backgroundColor: '#191919'
+        },
+        gameArea:{
+            flex: 1,
+            paddingTop: 10,
+            paddingBottom: 20,
+            width: '100%'
         }
 
     }
